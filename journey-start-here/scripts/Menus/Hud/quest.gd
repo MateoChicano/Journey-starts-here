a10bdf1b6dbde1ea9d86fc00e5506f93ed2deede
Pinou <chicmat4@gmail.com>
2025-12-04 11:07:38 +0100

    Update quest.gd

--- journey-start-here/scripts/Menus/Hud/quest.gd
+++ journey-start-here/scripts/Menus/Hud/quest.gd
@@ -5,6 +5,7 @@ extends HBoxContainer
 @onready var name_container : VBoxContainer = get_node("name_container")
 @onready var desc_container : Panel = get_node("desc_container")
 var quest_desc : String
+var quest_type : String
 
 func add_quest(quest_name : String) -> void :
 	var new_title : Label = Label.new()
@@ -13,6 +14,10 @@ func add_quest(quest_name : String) -> void :
 	new_desc.text = QuestManager.load_quest(quest_name)
 	name_container.add_child(new_title)
 	desc_container.add_child(new_desc)
+
+func give_item(given : item) -> void :
+	player.pick_item(given)
+
 	
 
 #ui_element
